358da612c21abd5bff4796a5f07a8a89727051a9
nem0z <maxime.bernard.dev@gmail.com>
2023-03-12 14:47:40 +0100

    feat (peer/handlers): implement handler for Version message

--- peer/handlers/handlers.go
+++ peer/handlers/handlers.go
@@ -1,14 +1,20 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/binary"
 
 	"github.com/nem0z/bitcoin-crawler/message"
 	"github.com/nem0z/bitcoin-crawler/peer"
 )
 
-func Version(peer *peer.Peer, msg *message.Message) {
-	fmt.Println("Deal with version", len(msg.Payload))
+func Version(ch chan *peer.Info) peer.Handler {
+	return func(p *peer.Peer, msg *message.Message) {
+		version := int32(binary.LittleEndian.Uint32(msg.Payload[:4]))
+		services := binary.LittleEndian.Uint64(msg.Payload[4:12])
+		relay := true
+
+		if version >= 70001 {
+			relay = msg.Payload[len(msg.Payload)-1] != 0
 }
 
 func Addr(peer *peer.Peer, msg *message.Message) {
