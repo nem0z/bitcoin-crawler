bd82e510e9e6070171d88299c5fd7e97de2f3e7b
nem0z <maxime.bernard.dev@gmail.com>
2024-01-12 14:40:19 +0100

    feat (crawler): implement crawler

---
+++ crawler/crawler.go
@@ -0,0 +1,122 @@
+package crawler
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	chandlers "github.com/nem0z/bitcoin-crawler/crawler/handlers"
+	"github.com/nem0z/bitcoin-crawler/peer"
+	phandlers "github.com/nem0z/bitcoin-crawler/peer/handlers"
+)
+
+type Crawler struct {
+	mu    sync.Mutex
+	nodes map[string]*peer.Node
+	out   chan *peer.Node
+	addr  chan *peer.Addr
+}
+
+func New(ip string, port int) (*Crawler, error) {
+	nodes := map[string]*peer.Node{}
+	chOut := make(chan *peer.Node)
+	chAddr := make(chan *peer.Addr)
+	crawler := &Crawler{sync.Mutex{}, nodes, chOut, chAddr}
+
+	addr := &peer.Addr{Ip: ip, Port: port}
+
+	crawler.Add(addr)
+
+	go crawler.HandleResult()
+	go crawler.HandleAddr(1000)
+
+	go crawler.StartMonitoring()
+
+	return crawler, nil
+}
+
+func (c *Crawler) add(addr string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.nodes[addr] = nil
+}
+
+func (c *Crawler) Exist(addr string) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	_, ok := c.nodes[addr]
+	return ok
+}
+
+func (c *Crawler) Add(addr *peer.Addr) {
+	if c.Exist(addr.String()) {
+		return
+	}
+
+	p, err := peer.New(addr.Ip, addr.Port, c.out)
+	if err != nil {
+		return
+	}
+	phandlers.DefaultRegister(p)
+	p.Register("addr", chandlers.Addr(c.addr))
+
+	err = p.Start()
+	if err != nil {
+		return
+	}
+
+	c.add(addr.String())
+}
+
+func (c *Crawler) HandleResult() {
+	for res := range c.out {
+		addr := res.Addr.String()
+		c.mu.Lock()
+		if _, ok := c.nodes[addr]; ok {
+			c.nodes[addr] = res
+		}
+		c.mu.Unlock()
+	}
+}
+
+func (c *Crawler) HandleAddr(n int) {
+	for i := 0; i < n; i++ {
+		go func() {
+			for addr := range c.addr {
+				c.Add(addr)
+			}
+		}()
+	}
+}
+
+func (c *Crawler) StartMonitoring() {
+	for {
+		time.Sleep(60 * time.Second)
+		c.Show()
+	}
+}
+
+func (c *Crawler) Show() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	cpt := len(c.nodes)
+	cptProcessed := 0
+	cptOk := 0
+
+	for _, node := range c.nodes {
+		if node != nil {
+			cptProcessed++
+			if node.Ping {
+				cptOk++
+			}
+		}
+	}
+
+	fmt.Println()
+	fmt.Println("Node discovered :", cpt)
+	fmt.Println("Node processed :", cptProcessed)
+	fmt.Println("Node ok :", cptOk)
+	fmt.Println()
+}
