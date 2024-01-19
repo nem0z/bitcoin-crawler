a86285674192869d3886c1014708ced42f89ff80
nem0z <maxime.bernard.dev@gmail.com>
2024-01-19 09:53:55 +0100

    feat: update main to use import/export

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/nem0z/bitcoin-crawler/crawler"
+	"github.com/nem0z/bitcoin-crawler/peer"
 	"github.com/nem0z/bitcoin-crawler/utils"
 )
 
-const peerIp = "2604:a880:4:1d0::3d:2000"
+const peerIp = "190.64.134.52"
 const peerPort = 8333
 
 func main() {
-	_, err := crawler.New(peerIp, peerPort)
+	addr := &peer.Addr{Ip: peerIp, Port: peerPort}
+	crawler, err := crawler.New(addr)
 	utils.Handle(err)
+
+	go crawler.Load("./export/nodes.json")
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		sig := <-signalCh
+		fmt.Printf("Received signal: %v\n", sig)
+
+		err = crawler.Export("./export/nodes.json")
+		utils.Handle(err)
+
+		os.Exit(0)
+	}()
 
 	select {}
 }
