14e0518ab3cbf665bd5f6261e0dd9f2acf36e816
nem0z <maxime.bernard.dev@gmail.com>
2024-01-26 11:23:21 +0100

    feat (database): update load request to get only nodes with last ping ok

--- database/load.go
+++ database/load.go
@@ -5,11 +5,16 @@ import (
 )
 
 func (db *DB) LoadAddrs(ok bool) ([]*peer.Addr, error) {
-	req := `SELECT n.ip, n.port 
-			FROM nodes n 
-			JOIN pings p
-			ON p.node_id = n.id
-			WHERE p.ok = ?`
+	req := `SELECT n.ip, n.port
+			FROM nodes n
+			JOIN pings p ON p.node_id = n.id
+			JOIN (
+				SELECT node_id, MAX(timestamp) AS latest_timestamp
+				FROM pings
+				WHERE ok = true
+				GROUP BY node_id
+			) latest_ping ON p.node_id = latest_ping.node_id AND p.timestamp = latest_ping.latest_timestamp
+			WHERE p.ok = true;`
 
 	rows, err := db.Query(req, ok)
 	if err != nil {
